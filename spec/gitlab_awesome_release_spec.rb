c1b23834dc6642c674d483fafbd6bf744ad3a26e
sue445 <sue445@sue445.net>
2015-10-03 00:41:51 +0900

    spec: red -> green

--- spec/gitlab_awesome_release_spec.rb
+++ spec/gitlab_awesome_release_spec.rb
@@ -1,11 +1,5 @@
-require 'spec_helper'
-
 describe GitlabAwesomeRelease do
   it 'has a version number' do
     expect(GitlabAwesomeRelease::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
