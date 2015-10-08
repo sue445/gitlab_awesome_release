cc157a3b0ab69500ecec98bcae6f939c165b4871
sue445 <sue445@sue445.net>
2015-10-08 10:03:58 +0900

    Bugfix: uninitialized constant GitlabAwesomeRelease::ArrayWithinExt (NameError)
    /Users/sueyoshi_go/.rbenv/versions/2.2.3/lib/ruby/gems/2.2.0/gems/gitlab_awesome_release-0.1.0.beta1/lib/gitlab_awesome_release/project.rb:5:in `<class:Project>': uninitialized constant GitlabAwesomeRelease::ArrayWithinExt (NameError)
      from /Users/sueyoshi_go/.rbenv/versions/2.2.3/lib/ruby/gems/2.2.0/gems/gitlab_awesome_release-0.1.0.beta1/lib/gitlab_awesome_release/project.rb:4:in `<module:GitlabAwesomeRelease>'
      from /Users/sueyoshi_go/.rbenv/versions/2.2.3/lib/ruby/gems/2.2.0/gems/gitlab_awesome_release-0.1.0.beta1/lib/gitlab_awesome_release/project.rb:1:in `<top (required)>'

--- lib/gitlab_awesome_release.rb
+++ lib/gitlab_awesome_release.rb
@@ -1,7 +1,6 @@
 require "active_support/all"
 require "gitlab_awesome_release/version"
 require "gitlab_awesome_release/project"
-require "gitlab_awesome_release/array_within_ext"
 require "gitlab"
 
 module GitlabAwesomeRelease
