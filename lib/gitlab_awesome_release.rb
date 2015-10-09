16fe96a964884a7c9874be905a42acdc49eadc1d
sue445 <sue445@sue445.net>
2015-10-10 00:25:30 +0900

    Add debug logger

--- lib/gitlab_awesome_release.rb
+++ lib/gitlab_awesome_release.rb
@@ -2,6 +2,7 @@ require "active_support/all"
 require "gitlab_awesome_release/version"
 require "gitlab_awesome_release/project"
 require "gitlab"
+# require "gitlab_awesome_release/gitlab_logging_ext"
 
 module GitlabAwesomeRelease
   # Your code goes here...
